648cedd68bb9a86e018e274cc875e6bae6e45a7d
nikolayjuminov <nikolayjuminov@yandex.ru>
2025-03-28 11:53:30 +0400

    Решение задачи

--- Seventh Task/Program.cs
+++ Seventh Task/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Tracing;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Write("Введите число для проверки: ");
+		bool isParsed = int.TryParse(Console.ReadLine(), out int userEntered);
+		if (isParsed == false)
+		{
+			Console.WriteLine("Введено некорректное значение, задача завершена...");
+		}
+		else
+		{
+			if (userEntered > Math.Pow(10, 9))
+			{
+				Console.WriteLine("Число слишком большое, задача завершена...");
+			}
+			else
+			{
+				if (userEntered < 0)
+				{
+					Console.WriteLine("Число отрицательное, задача завершена...");
+				}
+				else
+				{
+					bool result = Program.BodyTask(userEntered.ToString());
+					if (result)
+					{
+						Console.WriteLine("YES");
+					}
+					else
+					{
+						Console.WriteLine("NO");
+					}
+				}
+
+			}
+		}
+
+	}
+	private static bool BodyTask(string userEntered)
+	{
+		for (int i = 0; i < userEntered.Length / 2; i++)
+		{
+			if (userEntered[i] != userEntered[userEntered.Length - 1 - i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
